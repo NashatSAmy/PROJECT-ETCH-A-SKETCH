c691d678d9b6152139e472f227581bb3b3cfe822
Nashat Samy <nashat.msamy@gmail.com>
2022-06-15 21:38:38 +0200

    This update i have implemented the coloring system to allow the user to choose a color and start coloring with it

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PROJECT-ETCH-A-SKETCH
+
+My first thought on this project that i was going to use HTML canvas but, when i read the instruction of the project i noticed that they need to mimic a etch a sketch style so i started by making a grid out of divs by using CSS grid and allowed the user to enter their desired size as long as its not bigger than 100*100 next i will set up the drawing function.
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // Elements selectors.
 const grid = document.getElementById("grid-system");
-const gridSize = document.querySelectorAll("input");
+const gridSize = document.querySelectorAll("[name=grid-size]");
+const color = document.getElementsByName("pickAColor");
 
 // Function that clear the grid system.
-function clearGrid(){
+function clearGrid() {
   while (!grid.childElementCount <= 0) {
     grid.removeChild(grid.firstChild);
   }
@@ -13,18 +14,28 @@ function clearGrid(){
 function populateGridSystem(nX, nY) {
   nX = +gridSize[0].value;
   nY = +gridSize[1].value;
-  nX > 100 ? nX = 100 : nX = nX;
-  nY > 100 ? nY = 100 : nY = nY;
+  nX > 100 ? (nX = 100) : (nX = nX);
+  nY > 100 ? (nY = 100) : (nY = nY);
   if (nX <= 0 || nY <= 0) return;
-  clearGrid()
+  clearGrid();
   while (grid.childElementCount < nX * nY) {
     const gridDiv = document.createElement("div");
-    gridDiv.setAttribute("name", "grid-div")
+    gridDiv.setAttribute("name", "grid-div");
     grid.appendChild(gridDiv);
   }
   grid.style.gridTemplateRows = `repeat(${nX}, 1fr)`;
   grid.style.gridTemplateColumns = `repeat(${nY}, 1fr)`;
 }
 
+// Function that allow the user to start drawing.
+function startDrawing(e) {
+  if (
+    e.target.getAttribute("name") == null ||
+    !e.target.getAttribute("name") == "grid-div"
+  )
+    return;
+  e.target.style.backgroundColor = color[0].value;
+}
 
-gridSize.forEach(line => line.addEventListener("input", populateGridSystem));
+gridSize.forEach((line) => line.addEventListener("input", populateGridSystem));
+grid.addEventListener("mousemove", startDrawing);
