d57ccb127126de671c07a42feba82e9156033530
Nashat Samy <nashat.msamy@gmail.com>
2022-06-15 23:58:17 +0200

    Implemented the Rainbow color mode that allow users to paint with the colors of the rainbow randomly and it darkness 10% while changing color next i will implement the eraser and improve the UI also improving all of the css later.

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // Elements selectors.
 const grid = document.getElementById("grid-system");
-const gridSize = document.querySelectorAll("[name=grid-size]");
+const gridSize = document.querySelector("[name=grid-size]");
 const color = document.getElementsByName("pickAColor");
+const reset = document.getElementsByName("reset");
+const rainbowMode = document.getElementsByName("rainbow");
+
+// Global variables.
+let mouseDown = false;
+let rainbowModeOn = false;
+let hue = 0;
+let light = 100;
 
 // Function that clear the grid system.
 function clearGrid() {
@@ -11,31 +19,63 @@ function clearGrid() {
 }
 
 // Function that will populate the grid system with the right number of square divs.
-function populateGridSystem(nX, nY) {
-  nX = +gridSize[0].value;
-  nY = +gridSize[1].value;
-  nX > 100 ? (nX = 100) : (nX = nX);
-  nY > 100 ? (nY = 100) : (nY = nY);
-  if (nX <= 0 || nY <= 0) return;
+function populateGridSystem() {
+  nX = gridSize.value
   clearGrid();
-  while (grid.childElementCount < nX * nY) {
+  while (grid.childElementCount < nX * nX) {
     const gridDiv = document.createElement("div");
     gridDiv.setAttribute("name", "grid-div");
+    gridDiv.addEventListener("mouseenter", startDrawing);
+    gridDiv.addEventListener("mouseenter", toggleRainbowMode);
     grid.appendChild(gridDiv);
   }
   grid.style.gridTemplateRows = `repeat(${nX}, 1fr)`;
-  grid.style.gridTemplateColumns = `repeat(${nY}, 1fr)`;
+  grid.style.gridTemplateColumns = `repeat(${nX}, 1fr)`;
 }
 
 // Function that allow the user to start drawing.
 function startDrawing(e) {
+  if (rainbowModeOn == true) {
+    toggleRainbowMode(e);
+  }
   if (
     e.target.getAttribute("name") == null ||
-    !e.target.getAttribute("name") == "grid-div"
+    !e.target.getAttribute("name") == "grid-div" ||
+    mouseDown == false ||
+    rainbowModeOn == true
   )
     return;
   e.target.style.backgroundColor = color[0].value;
 }
 
-gridSize.forEach((line) => line.addEventListener("input", populateGridSystem));
-grid.addEventListener("mousemove", startDrawing);
+// Function that turnon the rainbow color mode.
+function toggleRainbowMode(e) {
+  if (
+    e.target.getAttribute("name") == null ||
+    !e.target.getAttribute("name") == "grid-div" ||
+    mouseDown == false ||
+    rainbowModeOn == false
+  )
+    return;
+  e.target.style.backgroundColor = `hsl(${hue}, 100%, ${light}%)`;
+  hue <= 359 ? hue++ : (hue = 0);
+  light <= 0 ? (light = 100) : (light -= 10);
+}
+
+
+// Events listeners.
+gridSize.addEventListener("input", populateGridSystem);
+grid.addEventListener("mousedown", () => (mouseDown = true));
+grid.addEventListener("mouseup", () => (mouseDown = false));
+
+grid.addEventListener("mousedown", startDrawing);
+grid.addEventListener("mousedown", toggleRainbowMode);
+
+rainbowMode[0].addEventListener(
+  "click",
+  () => (rainbowModeOn = !rainbowModeOn)
+);
+
+reset[0].addEventListener("click", populateGridSystem);
+
+window.onload = populateGridSystem()
